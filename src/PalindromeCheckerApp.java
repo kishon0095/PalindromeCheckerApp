66117ab16c23f26ac6880653abeec23393187dd3
Ajith200215 <as8890@srmist.edu.in>
2026-03-09 10:29:18 +0530

    src/PalindromeCheckerApp.java

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
+
+
+
