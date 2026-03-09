bfb266b7cfb6fc01e55d19fb028bf3acd43c0159
kishon0095 <kishon0095@gmail.com>
2026-03-09 10:37:19 +0530

    src/PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -13,7 +13,6 @@ public class PalindromeCheckerApp {
         String input = sc.nextLine();
 
         String reversed = "";
-
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
